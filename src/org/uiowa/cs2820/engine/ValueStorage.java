6445fec01f7cfa74dd79723b7ee78f448bb0018a
Joon Young <joonyoungkwon@hotmail.com>
2014-11-05 15:35:31 -0600

    ValueStorage initial commit

---
+++ src/org/uiowa/cs2820/engine/ValueStorage.java
@@ -0,0 +1,19 @@
+package org.uiowa.cs2820.engine;
+
+import java.util.ArrayList;
+
+public class ValueStorage {
+	
+	// load - read entire ValueLinked list
+	// convert into Arraylist of Identifiers
+	public static ArrayList<String> load() {
+	
+	}
+
+	// store - write entire
+	// Arraylist of Identifiers
+	public static void store (ArrayList<String> list){
+		
+	}
+
+}

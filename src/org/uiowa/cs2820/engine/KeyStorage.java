2792a785c3d80f9670024ae0283035e2a66b362d
cgrycki <christopher-grycki@uiowa.edu>
2014-11-05 14:49:37 -0600

    KeyStorage Class - Initial Commit
    Keystorage method added but not yet implemented.

---
+++ src/org/uiowa/cs2820/engine/KeyStorage.java
@@ -0,0 +1,30 @@
+package org.uiowa.cs2820.engine;
+
+public class KeyStorage {
+	// get - This is used exclusively by this class
+	// 		 to retrieve a node at the location specified
+	// 		 by areaNum
+	private Node get(int areaNum) {
+		// Find the node at areaNum and return it
+		Node returnNode = new Node(null, null);
+		return returnNode;
+	}
+	// put - This is used exclusively by this class 
+	// 		 to add a Node into storage
+	private void put(int areaNum, Node givenNode) {
+		// Insert the given node at areaNum
+		
+	}
+	// add - User calls this method to add the
+	//		 the given node into storage
+	public void add(Node givenNode) {
+		// Use allocate to find a place to put
+		// the given node
+	}
+	// del - User calls this method to remove the
+	//		 given node from storage
+	public void del(Node givenNode) {
+		// Find the given node and use
+		// allocate to free it
+	}
+}

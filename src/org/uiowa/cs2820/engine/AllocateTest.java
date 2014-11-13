b44f147a8e241c7d8f3bd0cfd83e482619e05fed
taozeyi <taozeyi@gmail.com>
2014-11-13 13:29:47 -0600

    simply test added

--- src/org/uiowa/cs2820/engine/AllocateTest.java
+++ src/org/uiowa/cs2820/engine/AllocateTest.java
@@ -2,17 +2,29 @@ package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AllocateTest {
+	@Before
+	public void deletion(){
+		File file = new File("test1.txt");
+		file.delete();
+	}
 	@Test
 	public void allocateTest(){
 		Allocate al = new Allocate("test1.txt");
 		al.allocate(10);
 		assertEquals(al.size(),54);
+		ArrayList<Integer> l = new ArrayList<>(Arrays.asList(1,2));
+		al.free(l);
+		assertEquals(al.size(),56);
+		al.allocate(58);
+		assertEquals(al.size(),62);
 	}
 
 }
--- src/org/uiowa/cs2820/engine/CheckpointTest.java
+++ src/org/uiowa/cs2820/engine/CheckpointTest.java
@@ -2,19 +2,27 @@ package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CheckpointTest {
+	@Before
+	public void deletion(){
+		File file = new File("test1.txt");
+		file.delete();
+	}
+	
 	@Test
 	public void saveTest(){
 		CheckPoint cp = new CheckPoint("test1.txt");
 		cp.save("1234");
-		//assertEquals(cp.isExists(),true);
-		//cp.restore("test1.txt");
 		assertEquals(cp.restore(),"1234");
-		
 		CheckPoint cp1 = new CheckPoint("test2.txt");
-		
+		String s="abcdefg";
+		cp1.save(s);
+		assertEquals(cp1.restore(),"abcdefg");
 	}
 
 }

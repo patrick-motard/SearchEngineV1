973900d8cd9480622a28df856cd44d17f65bd993
taozeyi <taozeyi@gmail.com>
2014-11-12 23:58:58 -0600

    temp

--- src/org/uiowa/cs2820/engine/Allocate.java
+++ src/org/uiowa/cs2820/engine/Allocate.java
@@ -1,19 +1,58 @@
 package org.uiowa.cs2820.engine;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+
 public class Allocate {
-	int index = 0;
-	// Initializer
-	protected Allocate() {
+	
+	private CheckPoint cp = null;
+	
+	//constructor of Allocate 
+	
+	public Allocate(String fileName)
+	{
 		
+		this.cp = new CheckPoint(fileName);
 	}
-	// allocate - Find a free space in memory and return
-	//			  the location of it.
-	protected int allocate() {
-		return index++;
+	
+    //method allocate will take a length(integer) from keyStorage or valueStorage
+	//as parameter and return an ArrayList of integer of valid position to save data
+	
+	public ArrayList<Integer> allocate(int size, String filename) 
+	{
+		ArrayList<Integer> position = new ArrayList<Integer>();
+		BitSet bs = null;
+		if (cp.isExists())
+		{
+			
+			bs = (BitSet) cp.restore(filename);
+		} 
+		else
+		{
+			
+			bs = new BitSet(size);
+		}
+		int init = 0;
+		while (position.size() < size)
+		{
+			
+			init = bs.nextClearBit(init);
+			bs.set(init);
+			position.add(init);
+		}
+		cp.save(bs);
+		return position;
 	}
-	// free - Take the location of an area in memory and
-	//		  mark it as free.
-	protected void free(int areaNum) {
+    //
+	
+	public void free(ArrayList<Integer> position, String filename) 
+	{
+		BitSet bs = (BitSet) cp.restore(filename);
+ 
+		for (Integer i : position)
+			bs.clear(i);
 		
+		cp.save(bs);
 	}
 }
+

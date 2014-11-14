f60dff07c52844f6edb622c874727316b4e930d4
Joon Young <joonyoungkwon@hotmail.com>
2014-11-13 23:22:15 -0600

    Update - Deleted errors

--- src/org/uiowa/cs2820/engine/ValueStorage.java
+++ src/org/uiowa/cs2820/engine/ValueStorage.java
@@ -6,21 +6,19 @@ import java.util.*;
 
 public class ValueStorage {
 	
-<<<<<<< HEAD
-	//public static int head;
-	//public static int size;
-	//public static long tail;
-	//public String identifier;
 	LinearFileDatabase db = null;
-	DiskSpace disk = new DiskSpace("keyvalue");
+	DiskSpace disk = new DiskSpace("keyvalues");
 	
-	public ValueStorage(LinearFileDatabase db) {
+	
+	public ValueStorage(LinearFileDatabase db){
 		this.db = db;
+		
 	}
 
 	public ArrayList<String> load(int front) throws IOException{
 		ArrayList<String> Identifiers = new ArrayList<String>();
-		//byte[] fileName = DiskSpace.readArea(front);
+		
+		// byte[] fileName = DiskSpace.readArea(front);
 		// front = location of first identifier on disk
 		return Identifiers;
 		boolean nextObject = true;
@@ -36,60 +34,21 @@ public class ValueStorage {
 				front = myObject.next;
 			}
 		}
+		// Use getObject and cast to a string to get strings
+		// put all strings in arraylist
+		// return arraylist
+		
 		for (LinkedListObject myLinkedListObject : objectList) {
 			String id = (String)myLinkedListObject.getObject();
 			Identifiers.add(id);
 		}
-		// Use getObject and cast to a string to get strings
-		// put all strings in arraylist
-		// return arraylist
-=======
-	LinearFileDatabase db = null;
-	DiskSpace disk = new DiskSpace("keyvalues");
-	
-	
-	public ValueStorage(LinearFileDatabase db){
-		this.db = db;
-		
+
+
 	}
-	
-	private LinkeListObject readObject(int front){
-		
-		int size;
-		LinkedListObject returnObject = null;
-		byte[] allBytes = disk.readArea(front);
-		byte[] sizeBytes = Arrays.copyOfRange(allBytes, 0, 2);
-		size = ((sizeBytes[0] & 0xff) << 8) + (sizeBytes[1] & 0xff);
-		
-		return returnObject;
->>>>>>> origin/master
-		
-	}
-	
-	
-	public static ArrayList<String> load(int front) throws IOException{
-		ArrayList<String> Identifiers = new ArrayList<String>();
-		//byte[] filename = DiskSpace.readArea(front);
-		
-		boolean nextObject = true;
-		ArrayList<LinkedListObject> objectList = new ArrayList<LinkedListObject>();
-		while (nextObject){
-			LinkedListObject myObject = readObject (front);
-			objectlist.add(myObject);
-			if(myObject.next == -1){
-				nextObject = false;
-			}
-			else {
-				front = myObject.next;
-			}
-		}
-	
-	}
-	
-	
+
 	public void store (ArrayList<String> list){
+		
 		// Call allocator with number of LinkedListObjects
-<<<<<<< HEAD
 		ArrayList<Integer> memorySpaces = this.db.allocator.allocate(list.size());
 		ArrayList<LinkedListObject> LinkedListObjectArray = new ArrayList<LinkedListObject>();
 		for (String id:list) {
@@ -100,10 +59,12 @@ public class ValueStorage {
 			}
 			else {
 				nextSpace = -1; // -1 = end of the list
+				
 			}
 			
 			LinkedListObject idListObject = new LinkedListObject(id,nextSpace,mySpace);
 			LinkedListObjectArray.add(idListObject);
+			
 		}
 		
 		// Serialize each LinkedListObject using Utility class
@@ -111,22 +72,6 @@ public class ValueStorage {
 		
 		// After all LinkedListObjects are converted to byte arrays
 		// Use Utility.appendSize to store byte[] size inside each byte[]
-=======
-		ArrayList<Integer> MemorySpaces = this.db.allocator.allocate(list.size());
-		ArrayList<LinkedListObject> LinkedListObjectArray = new ArrayList<LinkedListObject>();
-		
-		for (String id:list){
-			int mySpace = memorySpaces.get(0);
-			if (memeorySpaces.isEmpty() == false){
-				nextSpace = memeorySpaces.get(0);
-			
-			}
-			else{
-				nextSpace = -1; // -1 = end of the list
-			}
-			LinkedListObject idListObject = new LinkedListObject(id, )
-		}
->>>>>>> origin/master
 		
 		// Use diskspace writeArea method to write each byte array to disk
 		// disk.writeArea()

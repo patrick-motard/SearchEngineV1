5bea110b2c2090bcc735cb285e696c06d9e5f0f0
unknown <cgrycki@CS-S135.iowa.uiowa.edu>
2014-11-13 18:05:38 -0600

    Update Utility Features
    Utility will now deal with byte array
    sizes with 2 methods (one for reading,
    one for writing)

--- src/org/uiowa/cs2820/engine/Utility.java
+++ src/org/uiowa/cs2820/engine/Utility.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
 
 public class Utility {
 	public static byte[] convert(Object O) {
@@ -32,4 +33,27 @@ public class Utility {
 	  catch (Exception e) { };
 		return O;
 	  }
+	public static LinkedListObject readObject(int areaNum,DiskSpace disk) {
+		// Call diskspace readArea method with front - disk.readArea(front)
+		// first 2 bytes in byte array tell us how many bytes to read
+		// ex: 0000 0000 0000 0101 = 5 bytes
+		// whatwewant = byteArray[0:4]
+		// Use utility class on byte array to get object
+		
+		LinkedListObject returnObject = null;
+		byte[] allBytes = disk.readArea(areaNum);
+		byte[] sizeBytes = Arrays.copyOfRange(allBytes, 0, 2);
+		int size = ((sizeBytes[0] & 0xff) << 8) + (sizeBytes[1] & 0xff);
+		byte[] objectBytes = Arrays.copyOfRange(allBytes, 2, size+1);
+		returnObject = (LinkedListObject)Utility.revert(objectBytes);
+		return returnObject;
+		
+	}
+	
+	public static byte[] appendSize(byte[] data) {
+		// get size of each byte array
+		// In beginning of each bye array, store size in 2 bytes 
+		byte[] returnBytes = null;
+		return returnBytes;
+	}
 }

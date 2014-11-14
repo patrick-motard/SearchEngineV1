a589ab2b15243652c369a8951fa394e4a9e6858d
unknown <cgrycki@CS-S135.iowa.uiowa.edu>
2014-11-13 18:07:53 -0600

    Update ValueStorage
    Value Storage will store identifiers
    in a linked list. The Store method
    needs to be finished.

--- src/org/uiowa/cs2820/engine/ValueStorage.java
+++ src/org/uiowa/cs2820/engine/ValueStorage.java
@@ -7,23 +7,73 @@ import java.util.*;
 
 public class ValueStorage {
 	
-	public static int head;
-	public static int size;
-	public static long tail;
-	public String identifier;
+	//public static int head;
+	//public static int size;
+	//public static long tail;
+	//public String identifier;
+	LinearFileDatabase db = null;
+	DiskSpace disk = new DiskSpace("keyvalue");
 	
-	public static ArrayList<String> load(long front) throws IOException{
+	public ValueStorage(LinearFileDatabase db) {
+		this.db = db;
+	}
+
+	public ArrayList<String> load(int front) throws IOException{
 		ArrayList<String> Identifiers = new ArrayList<String>();
-		byte[] fileName = DiskSpace.readArea(front);
-		
+		//byte[] fileName = DiskSpace.readArea(front);
+		// front = location of first identifier on disk
+		boolean nextObject = true;
+		ArrayList<LinkedListObject> objectList = new ArrayList<LinkedListObject>();
+		while (nextObject) {
+			LinkedListObject myObject = Utility.readObject(front,disk);
+			// myObject should be a LinkedListObject with a pointer to next LinkedListObject
+			objectList.add(myObject);
+			if (myObject.next == -1) {
+				nextObject = false;
+			}
+			else {
+				front = myObject.next;
+			}
+		}
+		for (LinkedListObject myLinkedListObject : objectList) {
+			String id = (String)myLinkedListObject.getObject();
+			Identifiers.add(id);
+		}
+		// Use getObject and cast to a string to get strings
+		// put all strings in arraylist
+		// return arraylist
 		
 		return Identifiers;
 	
 	}
 	
 	
-	public static void store (ArrayList<String> list){
+	public void store (ArrayList<String> list){
+		// Call allocator with number of LinkedListObjects
+		ArrayList<Integer> memorySpaces = this.db.allocator.allocate(list.size());
+		ArrayList<LinkedListObject> LinkedListObjectArray = new ArrayList<LinkedListObject>();
+		for (String id:list) {
+			int mySpace = memorySpaces.remove(0);
+			int nextSpace;
+			if (memorySpaces.isEmpty() == false) {
+				nextSpace = memorySpaces.get(0);
+			}
+			else {
+				nextSpace = -1; // -1 = end of the list
+			}
+			
+			LinkedListObject idListObject = new LinkedListObject(id,nextSpace,mySpace);
+			LinkedListObjectArray.add(idListObject);
+		}
 		
+		// Serialize each LinkedListObject using Utility class
+		//byte[] bytes = Utility.convert(exampleLinkedListObject);
+		
+		// After all LinkedListObjects are converted to byte arrays
+		// Use Utility.appendSize to store byte[] size inside each byte[]
+		
+		// Use diskspace writeArea method to write each byte array to disk
+		// disk.writeArea()
 	}
 
 }
